fde510d22e2738bfba58df812545548afb8ec6a7
zackr5759 <51389011+zackr5759@users.noreply.github.com>
2022-02-19 17:19:17 -0800

    touched up main function, started FindString
    I'm not 100% sure of the inputs to findString because of where the instructions get cut off

--- part2.py
+++ part2.py
@@ -1,5 +1,24 @@
 # Author(s)
 
+from queue import Queue
+import queue
+from sys import maxsize
+
 class part2:
     def delta(p, q, r):
         return (10 * p + q) % r
+
+
+    def FindString(DFA, S, k):
+
+        Q = queue(maxsize = 99999)
+        visited = []
+        parent = []
+        
+        for i in range(0, k-1):
+            visited[i] = 0
+        for i in range(0, k-1):
+            parent[i] = -1
+
+        
+
--- project1.py
+++ project1.py
@@ -2,8 +2,11 @@
 from part1 import part1
 
 def main():
+    print("Enter an integer for the length of the string")
+    length = input()
+
     DFACount = part1()
-    print(DFACount.count(56))
+    print("Number of possible strings of size " + str(length) + " that map to our language: " + str(DFACount.count(int(length))))
 
 if __name__ == "__main__":
     main()
